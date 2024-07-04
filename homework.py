0b51e50b1a101e64a0a962f4248d9baadbe11f9e
1ceHat <artemdudorov2015@gmail.com>
2024-07-04 21:51:32 +0500

    homework2

---
+++ homework.py
@@ -0,0 +1,6 @@
+count_done_homeworks = 12
+spent_hours = 1.5
+course_name = 'Python'
+hours_for_exercise = spent_hours/count_done_homeworks
+print(f"Курс: {course_name}, всего задач: {count_done_homeworks},"
+f"затрачено часов: {spent_hours}, среднее время выполнения: {hours_for_exercise} часа")
