123432f0e50a9b1734a05ce5fc67ea0da5f87c44
1ceHat <artemdudorov2015@gmail.com>
2024-07-04 22:38:40 +0500

    homework 6

--- homework.py
+++ homework.py
@@ -1,6 +1,13 @@
-immutable_var = (1, "Arr", True)
-print(f"Immutable tuple: {immutable_var}")
-mutable_var = [1, "Arr", True]
-print(f"Mutable list: {mutable_var}")
-mutable_var[0] = 2
-print(f"New mutable list: {mutable_var}")
+my_dict = {'Artem':2004, 'Dasha':2004}
+print(f"Dict: {my_dict}")
+print(f"Existing value: {my_dict['Artem']}")
+my_dict.update({'Nikita':2003, 'Polina':2007})
+print(f"Deleted value: {my_dict.pop('Dasha')}")
+print(f"Modified dictionary: {my_dict}")
+
+my_set = {1,2,1,'Artem', 'Dasha', 'Artem'}
+print(f"\nSet: {my_set}")
+my_set.add(3)
+my_set.add(False)
+my_set.discard(1)
+print(f"Modified set: {my_set}")
