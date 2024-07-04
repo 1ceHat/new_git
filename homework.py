027005d6b454c33fb695d752a8f9718b31ae1d82
1ceHat <artemdudorov2015@gmail.com>
2024-07-04 22:01:09 +0500

    homework 4

--- homework.py
+++ homework.py
@@ -1,8 +1,6 @@
-name = "Artem"
-print(f"Name: {name}")
-age = 20
-print(f"Age: {age}")
-new_age = age + 1
-print(f"New age: {new_age}")
-is_student = True
-print(f"Is student: {is_student}")
+my_string = input("Введите что-нибудь: ")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
