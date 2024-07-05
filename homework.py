d0be3d0a47a2554de556f1584587172b53153f57
1ceHat <artemdudorov2015@gmail.com>
2024-07-05 10:49:35 +0500

    module2 2

--- homework.py
+++ homework.py
@@ -1,17 +1,10 @@
-import random as rnd
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
 
-students = {'John', 'Doe', 'Sonic', 'Star', 'Uaou', 'Aaaa'}
-sorted_students = sorted(tuple(students))
-grades = []
-average_marks = {}
-
-for student in sorted_students:
-    grades.append([])
-    for i in range(rnd.randint(2,10)):
-        grades[-1].append(rnd.randint(2,5))
-    print(grades[-1])
-    average_marks[student] = round(sum(grades[-1])/len(grades[-1]), 2)
-
-
-for key, value in average_marks.items():
-    print(f"{key}'s average mark is {value}")
+if first == second and first == third:
+    print(3)
+elif (first == second and first != third) or (first != second and first == third) or (third == second and first != third):
+    print(2)
+elif first != second and first != third and second != third:
+    print(0)
