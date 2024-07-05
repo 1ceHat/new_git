26ac90de97d276e0e2f8194430cdc58a2900df34
1ceHat <artemdudorov2015@gmail.com>
2024-07-05 10:07:54 +0500

    homework 4 complete

--- homework.py
+++ homework.py
@@ -1,13 +1,7 @@
-my_dict = {'Artem':2004, 'Dasha':2004}
-print(f"Dict: {my_dict}")
-print(f"Existing value: {my_dict['Artem']}")
-my_dict.update({'Nikita':2003, 'Polina':2007})
-print(f"Deleted value: {my_dict.pop('Dasha')}")
-print(f"Modified dictionary: {my_dict}")
-
-my_set = {1,2,1,'Artem', 'Dasha', 'Artem'}
-print(f"\nSet: {my_set}")
-my_set.add(3)
-my_set.add(False)
-my_set.discard(1)
-print(f"Modified set: {my_set}")
+my_string = input("Введите что-нибудь: ")
+print(f"Количество введённых символов: {len(my_string)}")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
