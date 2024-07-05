cdae5bbeb6c0d84d2627309fd755ddec69434e74
1ceHat <artemdudorov2015@gmail.com>
2024-07-05 20:00:55 +0500

    module2 4

--- homework.py
+++ homework.py
@@ -1,11 +1,20 @@
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-i = 0
-while i < len(my_list):
-    if my_list[i] > 0:
-        print(my_list[i])
-    if my_list[i] < 0:
-        break
-    if my_list[i] == 0:
-        i += 1
-        continue
-    i += 1
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numbers.remove(1)
+primes = []
+not_primes = []
+
+for number in numbers:
+    sqrt = int(number**0.5)
+    del_count = 1
+    for i in range(1, sqrt+1):
+        if number % i == 0:
+            del_count += 1
+        if del_count > 2:
+            break
+    if del_count < 3:
+        primes.append(number)
+    else:
+        not_primes.append(number)
+
+print(f"Простые числа {primes}")
+print(f"Не простые числа {not_primes}")
