628b41da0dbf382772e0be1b48209110c2bd3992
1ceHat <artemdudorov2015@gmail.com>
2024-07-05 10:18:29 +0500

    module1hard

--- homework.py
+++ homework.py
@@ -1,7 +1,17 @@
-my_string = input("Введите что-нибудь: ")
-print(f"Количество введённых символов: {len(my_string)}")
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
+import random as rnd
+
+students = {'John', 'Doe', 'Sonic', 'Star', 'Uaou', 'Aaaa'}
+sorted_students = sorted(tuple(students))
+grades = []
+average_marks = {}
+
+for student in sorted_students:
+    grades.append([])
+    for i in range(rnd.randint(2,10)):
+        grades[-1].append(rnd.randint(2,5))
+    print(grades[-1])
+    average_marks[student] = round(sum(grades[-1])/len(grades[-1]), 2)
+
+
+for key, value in average_marks.items():
+    print(f"{key}'s average mark is {value}")
