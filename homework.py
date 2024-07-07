b5a1c54c31f37c9e2de023bc78889daf4b589c2b
1ceHat <artemdudorov2015@gmail.com>
2024-07-07 19:24:09 +0500

    module3 4

--- homework.py
+++ homework.py
@@ -1,18 +1,13 @@
-def print_params(a=1, b='stroka', c=True):
-    print(a, b, c)
+def single_root_words (root_word, *other_words):
+    same_words = []
+    for word in other_words:
+        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
+            same_words.append(word)
+
+    return same_words
 
 
-
-print_params(b=25)
-print_params(c=[1,2,3])
-
-values_list = ['myau', True, 1.24]
-values_dict = {'a': 'myau2', 'b': False, 'c': 1.54}
-
-print_params(*values_list)
-print_params(**values_dict)
-
-values_list_2 = [20.1, 1]
-
-print_params(*values_list_2, 42)
-
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
