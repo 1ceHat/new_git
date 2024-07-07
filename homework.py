57691e07b4d37ecc51d005300180d712674b9928
1ceHat <artemdudorov2015@gmail.com>
2024-07-07 18:40:10 +0500

    module3 1

--- homework.py
+++ homework.py
@@ -1,18 +1,34 @@
-import random as rnd
-
-number = rnd.randint(3, 20)
-password = ''
-deliters = set()
-for i in range(1, int(number**0.5)+1):
-    if number%i==0:
-        deliters.add(i)
-        deliters.add(number//i)
-sorted(deliters)
+calls = 0
 
 
-for num in range(1,number//2+1):
-    for deliter in deliters:
-        if num<deliter-num:
-            password += str(num)+str(deliter-num)
+def count_calls():
+    global calls
+    calls += 1
 
-print(number, password)
+
+def string_info(string='string'):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string_info='string', list_to_search=['stri', 'strin', 'string1']):
+    count_calls()
+    # перевели пришедшие данные в строковый тип с нижним написанием
+    for i in range(len(list_to_search)):
+        list_to_search[i] = str(list_to_search[i]).lower()
+    string_info = str(string_info).lower()
+
+    if string_info in list_to_search:
+        return True
+    else:
+        return False
+
+
+list1 = ['ban', 'BaNaN', 'urBAN']
+list2 = ['recycling', 'cyclic']
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', list1))
+print(is_contains('cycle', list2))
+print(calls)
