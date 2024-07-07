758eb27830f1a646cae828c59114be29accea057
1ceHat <artemdudorov2015@gmail.com>
2024-07-07 19:10:41 +0500

    module3 3

--- homework.py
+++ homework.py
@@ -1,29 +1,18 @@
-def send_email(message, recipient, *, sender="university.help@gmail.com"):
+def print_params(a=1, b='stroka', c=True):
+    print(a, b, c)
 
 
-    domains = ['.com', '.ru', '.net'] # возможные почтовые домены
-    recipients_domain = recipient[recipient.rfind('.'):] # домен получателя
-    senders_domain = sender[sender.rfind('.'):] # домен отправителя
 
-    # проверка адреса отправителя и получателя
-    if ('@' not in recipient or recipients_domain not in domains) \
-            or ('@' not in sender or senders_domain not in domains):
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}!')
-        return False
+print_params(b=25)
+print_params(c=[1,2,3])
 
-    if recipient == sender:
-        print('Нельзя отправить письмо самому себе!')
-        return False
+values_list = ['myau', True, 1.24]
+values_dict = {'a': 'myau2', 'b': False, 'c': 1.54}
 
-    if sender == "university.help@gmail.com":
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
-        return True
-    else:
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
-        return True
+print_params(*values_list)
+print_params(**values_dict)
 
+values_list_2 = [20.1, 1]
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+print_params(*values_list_2, 42)
+
