c7a0375b221f5712999d3689a48715a6d48e9b2d
1ceHat <artemdudorov2015@gmail.com>
2024-07-07 19:48:02 +0500

    module3 hard

--- homework.py
+++ homework.py
@@ -1,11 +1,30 @@
-def get_multiplied_digits(number):
-    str_number = str(number)
-    first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
+def calculate_structure_sum(data_structer):
+    if isinstance(data_structer, list) or isinstance(data_structer, tuple) or isinstance(data_structer, set):
+        sum_len = 0
+        for value in data_structer:
+            sum_len += calculate_structure_sum(value)
+        return sum_len
+    elif isinstance(data_structer, dict):
+        sum_len = 0
+        for key, value in data_structer.items():
+            len_key = calculate_structure_sum(key)
+            len_value = calculate_structure_sum(value)
+            sum_len += len_value+len_key
+        return sum_len
+    elif isinstance(data_structer, str):
+        return len(data_structer)
+    elif isinstance(data_structer, int) or isinstance(data_structer, float):
+        return data_structer
 
 
-result = get_multiplied_digits(40203)
-print(result)
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+sum_len = calculate_structure_sum(data_structure)
+print(sum_len)
