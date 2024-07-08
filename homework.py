3143b68f00939eb9aae8986c1069265017e58e71
1ceHat <artemdudorov2015@gmail.com>
2024-07-09 00:38:35 +0500

    module6 3

--- homework.py
+++ homework.py
@@ -1,54 +1,46 @@
-from random import choice
+class Horse:
+
+    def __init__(self, x_distance=0, sound='Frrr'):
+        self.x_distance = x_distance
+        self.sound = sound
+        super().__init__()
+
+    def run(self, dx):
+        self.x_distance += dx
 
 
-class Vehicle:
+class Eagle:
 
-    __COLOR_VARIANTS = ['green', 'gray', 'serebro']
+    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
+        self.y_distance = y_distance
+        self.sound = sound
 
-    def __init__(self, owner, model, color, engine_power):
-        self.owner = owner
-        self.__model = model
-        self.__engine_power = engine_power
-        if color.lower() in self.__COLOR_VARIANTS:
-            self.__color = color
-        else:
-            self.__color = choice(self.__COLOR_VARIANTS)
-            print(f'Невозможно создать машину с цветом {color}. Машина была создана в цвете {self.__color}')
-
-    def get_model(self):
-        return f'Модель: {self.__model}'
-
-    def get_horsepower(self):
-        return f'Мощность двигателя: {self.__engine_power}'
-
-    def get_color(self):
-        return f'Цвет: {self.__color}'
-
-    def print_info(self):
-        print(f"{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}" \
-               f"\nВладелец: {self.owner}")
-
-    def set_color(self, new_color):
-        if new_color.lower() in self.__COLOR_VARIANTS:
-            self.__color = new_color
-        else:
-            print(f"Нельзя изменить цвет на '{new_color}'")
+    def fly(self, dy):
+        self.y_distance += dy
 
 
-class Sedan(Vehicle):
-    __PASSENGERS_LIMIT = 5
+class Pegasus(Horse, Eagle):
+
+    def __init__(self):
+        super().__init__()
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        print(self.sound)
 
 
-# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
-vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+p1 = Pegasus()
 
-# Изначальные свойства
-vehicle1.print_info()
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
 
-# Меняем свойства (в т.ч. вызывая методы)
-vehicle1.set_color('Pink')
-vehicle1.set_color('serebro')
-vehicle1.owner = 'Vasyok'
-
-# Проверяем что поменялось
-vehicle1.print_info()
+p1.voice()
