3d77557d3c55dfaa46caee16b4e07c9a9536c85d
1ceHat <artemdudorov2015@gmail.com>
2024-07-08 12:35:19 +0500

    module4 2

--- homework.py
+++ homework.py
@@ -1,11 +1,10 @@
-from fake_math import divide as div_fake
-from true_math import divide as div_true
+def test_function():
+    def inner_function():
+        print("Я в области видимости test_function")
 
-result1 = div_fake(69, 3)
-result2 = div_fake(3, 0)
-result3 = div_true(49, 7)
-result4 = div_true(15, 0)
-print(result1)
-print(result2)
-print(result3)
-print(result4)
+
+    inner_function()
+
+
+test_function()
+# inner_function() # Выдаёт ошибку, т.к. inner_function существует только в области видимости test_function
