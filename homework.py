a34c385fc72ec7f0cf016a0a2bf78f3b0470c910
1ceHat <artemdudorov2015@gmail.com>
2024-07-09 02:12:19 +0500

    module6 hard

--- homework.py
+++ homework.py
@@ -1,46 +1,107 @@
-class Horse:
-
-    def __init__(self, x_distance=0, sound='Frrr'):
-        self.x_distance = x_distance
-        self.sound = sound
-        super().__init__()
-
-    def run(self, dx):
-        self.x_distance += dx
+import math
 
 
-class Eagle:
+class Figure:
 
-    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
-        self.y_distance = y_distance
-        self.sound = sound
+    sides_count = 0
 
-    def fly(self, dy):
-        self.y_distance += dy
+    def __init__(self, colors: list = None, sides: list = None,  filled: bool = False):
+        self.filled = filled
+        if self.__is_valid_sides(*sides):
+            self.__sides = sides
+        else:
+            self.__sides = [1]*self.sides_count
+
+        if self.__is_valid_color(*colors):
+            self.__color = list(colors)
+        else:
+            self.__color = [0]*3
+
+    def get_color(self):
+        return self.__color
+
+    def __is_valid_color(self, r: int, g: int, b: int):
+        if r in range(256) and g in range(256) and b in range(256):
+            return True
+        else:
+            return False
+
+    def set_color(self, r: int, g: int, b: int):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
+
+    def __is_valid_sides(self, *sides):
+        if len(sides) == self.sides_count:
+            for side in sides:
+                if side <= 0 or not isinstance(side, int):
+                    return False
+            return True
+        return False
+
+    def get_sides(self):
+        return self.__sides
+
+    def __len__(self):
+        return sum(self.__sides)
+
+    def set_sides(self, *new_sides):
+        if self.__is_valid_sides(*new_sides):
+            self.__sides = list(new_sides)
 
 
-class Pegasus(Horse, Eagle):
+class Circle(Figure):
 
-    def __init__(self):
-        super().__init__()
+    sides_count = 1
 
-    def move(self, dx, dy):
-        self.run(dx)
-        self.fly(dy)
+    def __init__(self, colors: list = None, sides: list = None):
+        super().__init__(colors, [sides])
+        self.__radius = len(self)/(2*math.pi)
 
-    def get_pos(self):
-        return self.x_distance, self.y_distance
-
-    def voice(self):
-        print(self.sound)
+    def get_square(self):
+        return math.pi*(self.__radius**2)
 
 
-p1 = Pegasus()
+class Triangle(Figure):
 
-print(p1.get_pos())
-p1.move(10, 15)
-print(p1.get_pos())
-p1.move(-5, 20)
-print(p1.get_pos())
+    sides_count = 3
 
-p1.voice()
+    def __init__(self, colors: list = None, sides: list = None):
+        super().__init__(colors, sides)
+        self.__height = math.sqrt(self.get_sides()[0]**2 - (self.get_sides()[0]/2)**2)
+
+    def get_square(self):
+        half_per = len(self)/2
+        return math.sqrt(half_per * (half_per-self.get_sides()[0]) * (half_per-self.get_sides()[1]) *
+                         (half_per-self.get_sides()[2]))
+
+class Cube(Figure):
+
+    sides_count = 12
+
+    def __init__(self, colors: list = None, side: int = 1):
+        super().__init__(colors, [side]*self.sides_count)
+
+    def get_volume(self):
+        return self.get_sides()[0]**3
+
+
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15) # Изменится
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
