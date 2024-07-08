629889d534fc67a54ab14871c225b7a0dd397f41
1ceHat <artemdudorov2015@gmail.com>
2024-07-08 13:39:12 +0500

    module5 1

--- homework.py
+++ homework.py
@@ -1,10 +1,19 @@
-def test_function():
-    def inner_function():
-        print("Я в области видимости test_function")
+class House:
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует!")
+        else:
+            for i in range(1, new_floor+1):
+                print(i)
+            print(f"Вы приехали на {new_floor} этаж.")
 
 
-    inner_function()
-
-
-test_function()
-# inner_function() # Выдаёт ошибку, т.к. inner_function существует только в области видимости test_function
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
