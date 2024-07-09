912b1eadb60315d62ad113e3ac6609117612e7e4
1ceHat <artemdudorov2015@gmail.com>
2024-07-09 18:18:43 +0500

    module7 2

--- homework.py
+++ homework.py
@@ -1,43 +1,23 @@
-class Product:
-
-    def __init__(self, name: str, weight: float, category: str):
-        self.name = name
-        self.weight = weight
-        self.category = category
-
-    def __str__(self):
-        return f"{self.name}, {self.weight}, {self.category}"
-
-
-class Shop:
-
-    def __init__(self, file_name: str):
-        self.__file_name = file_name
-
-    def get_products(self):
-        file = open(self.__file_name, 'r')
-        text = file.read()
-        file.close()
-        return text
-
-    def add(self, *products: Product):
-        for product in products:
-            file = open(self.__file_name, 'a')
-            text = self.get_products()
-            if product.name.lower() not in text.lower():
-                file.write(str(product)+'\n')
-            else:
-                print(f'Продукт {product.name} уже есть в магазине' )
-            file.close()
+def custom_write(file_name, strings):
+    dict_write = {}
+    file = open(file_name, 'w', encoding='utf-8')
+    i = 0
+    for string in strings:
+        i += 1
+        current_pos = file.tell()
+        dict_write[(i, current_pos)] = string
+        file.write(string+'\n')
+    file.close()
+    return dict_write
 
 
-s1 = Shop('products.txt')
-p1 = Product('Potato', 50.5, 'Vegetables')
-p2 = Product('Spaghetti', 3.4, 'Groceries')
-p3 = Product('Potato', 5.5, 'Vegetables')
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
 
-print(p2) # __str__
-
-s1.add(p1, p2, p3)
-
-print(s1.get_products())
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
