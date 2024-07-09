d071b9af239b9c0e378fb9ec7f774717eec0d1bf
1ceHat <artemdudorov2015@gmail.com>
2024-07-09 11:31:48 +0500

    module6 1

--- homework.py
+++ homework.py
@@ -1,107 +1,64 @@
-import math
+class Animal:
+
+    def __init__(self, name, alive=True, fed=False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
 
 
-class Figure:
+class Plant:
 
-    sides_count = 0
+    def __init__(self, name, edible=False):
+        self.name = name
+        self.edible = edible
 
-    def __init__(self, colors: list = None, sides: list = None,  filled: bool = False):
-        self.filled = filled
-        if self.__is_valid_sides(*sides):
-            self.__sides = sides
+
+class Mammal(Animal):
+
+    def eat(self, food: Plant):
+        if food.edible:
+            self.fed = True
+            print(f"{self.name} съел {food.name}")
         else:
-            self.__sides = [1]*self.sides_count
+            self.alive = False
+            print(f"{self.name} не стал есть {food.name} и умер")
 
-        if self.__is_valid_color(*colors):
-            self.__color = list(colors)
+
+class Predator(Animal):
+
+    def eat(self, food: Plant):
+        if food.edible:
+            self.fed = True
+            print(f"{self.name} съел {food.name}")
         else:
-            self.__color = [0]*3
-
-    def get_color(self):
-        return self.__color
-
-    def __is_valid_color(self, r: int, g: int, b: int):
-        if r in range(256) and g in range(256) and b in range(256):
-            return True
-        else:
-            return False
-
-    def set_color(self, r: int, g: int, b: int):
-        if self.__is_valid_color(r, g, b):
-            self.__color = [r, g, b]
-
-    def __is_valid_sides(self, *sides):
-        if len(sides) == self.sides_count:
-            for side in sides:
-                if side <= 0 or not isinstance(side, int):
-                    return False
-            return True
-        return False
-
-    def get_sides(self):
-        return self.__sides
-
-    def __len__(self):
-        return sum(self.__sides)
-
-    def set_sides(self, *new_sides):
-        if self.__is_valid_sides(*new_sides):
-            self.__sides = list(new_sides)
+            self.alive = False
+            print(f"{self.name} не стал есть {food.name} и умер")
 
 
-class Circle(Figure):
-
-    sides_count = 1
-
-    def __init__(self, colors: list = None, sides: list = None):
-        super().__init__(colors, [sides])
-        self.__radius = len(self)/(2*math.pi)
-
-    def get_square(self):
-        return math.pi*(self.__radius**2)
+class Flower(Plant):
+    pass
 
 
-class Triangle(Figure):
+class Fruit(Plant):
 
-    sides_count = 3
-
-    def __init__(self, colors: list = None, sides: list = None):
-        super().__init__(colors, sides)
-        self.__height = math.sqrt(self.get_sides()[0]**2 - (self.get_sides()[0]/2)**2)
-
-    def get_square(self):
-        half_per = len(self)/2
-        return math.sqrt(half_per * (half_per-self.get_sides()[0]) * (half_per-self.get_sides()[1]) *
-                         (half_per-self.get_sides()[2]))
-
-class Cube(Figure):
-
-    sides_count = 12
-
-    def __init__(self, colors: list = None, side: int = 1):
-        super().__init__(colors, [side]*self.sides_count)
-
-    def get_volume(self):
-        return self.get_sides()[0]**3
+    def __init__(self, name):
+        self.name = name
+        self.edible = True
 
 
-circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
-cube1 = Cube((222, 35, 130), 6)
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
 
-# Проверка на изменение цветов:
-circle1.set_color(55, 66, 77) # Изменится
-print(circle1.get_color())
-cube1.set_color(300, 70, 15) # Не изменится
-print(cube1.get_color())
+print(a1.name)
+print(p1.name)
 
-# Проверка на изменение сторон:
-cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
-print(cube1.get_sides())
-circle1.set_sides(15) # Изменится
-print(circle1.get_sides())
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
 
-# Проверка периметра (круга), это и есть длина:
-print(len(circle1))
-
-# Проверка объёма (куба):
-print(cube1.get_volume())
+# Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
