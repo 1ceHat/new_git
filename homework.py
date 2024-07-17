cd4fb130db2c6be86d873f5b190d66819f9b7c67
1ceHat <artemdudorov2015@gmail.com>
2024-07-18 03:00:37 +0500

    module9 2

--- homework.py
+++ homework.py
@@ -1,9 +1,11 @@
-def apply_all_func(int_list, *functions):
-    result = {}
-    for function in functions:
-        result[function.__name__] = function(int_list)
-    return result
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(x) for x in first_strings if len(x) >= 5]
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {x: len(x) for x in (first_strings + second_strings) if not len(x) % 2}
 
 
-print(apply_all_func([6, 20, 15, 9], max, min))
-print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(first_result)
+print(second_result)
+print(third_result)
