14d2f5836182633d974df487649e05efe6f972a8
1ceHat <artemdudorov2015@gmail.com>
2024-07-18 04:12:41 +0500

    module9 4

--- homework.py
+++ homework.py
@@ -1,11 +1,34 @@
-first = ['Strings', 'Student', 'Computers']
-second = ['Строка', 'Урбан', 'Компьютер']
+from random import choice
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+result_lambda = list(map(lambda x, y: x == y, first, second))
+print(result_lambda)
+def get_advanced_writer(file_name):
+
+    def write_everything(*data_set):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for data in data_set:
+                file.write(str(data)+'\n')
+
+    return write_everything
 
 
-first_result = (len(x)-len(y) for x, y in zip(first, second) if len(x) != len(y))
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
 
-max_len = max(len(first), len(second))
-second_result = (len(first[x]) == len(second[x]) for x in range(max_len))
 
-print(list(first_result))
-print(list(second_result))
+class MysticBall:
+
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
