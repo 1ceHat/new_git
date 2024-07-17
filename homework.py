f825a8d48ea1315699a46a7576f4fbd82e06dfd6
1ceHat <artemdudorov2015@gmail.com>
2024-07-17 20:07:09 +0500

    module8 1

--- homework.py
+++ homework.py
@@ -1,15 +1,12 @@
-import os
-import time
+def add_everything_up(a, b):
+    try:
+        result = a + b
+    except TypeError:
+        result = str(a) + str(b)
+    finally:
+        return result
 
-current_dir = os.getcwd()
-for root, dirs, files in os.walk(current_dir):
-    if '.git' in root or '.idea' in root or '__' in root:
-        continue
-    for file in files:
-        file_path = os.path.join(root, file)
-        file_time = os.path.getmtime(file)
-        file_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(file_time))
-        file_size = os.path.getsize(file)
-        parent_dir = os.path.dirname(file)
-        print(f"Обнаружен файл: {file}, Путь: {file_path}, Размер: {file_size}, "
-              f"Время изменения: {file_time}, Родительская директория: {parent_dir}")
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
