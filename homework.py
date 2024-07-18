69852fdd8576e3d6a2bd7f58651f1d51fabd522e
1ceHat <artemdudorov2015@gmail.com>
2024-07-19 02:56:10 +0500

    module10 1

--- homework.py
+++ homework.py
@@ -1,26 +1,41 @@
-def is_prime(func):
-    def wrapper(*args):
-        res = func(*args)
-        num_sqrt = int(res ** 0.5)
-        prime = 'Простое'
-        delit = []
-        for i in range(1, num_sqrt + 1):
-            if res % i == 0:
-                delit.append(i)
-                delit.append(res // i)
-            if len(delit) > 2:
-                prime = 'Составное'
-                break
-        print(prime)
-        return res
-
-    return wrapper
+from threading import Thread
+import time
 
 
-@is_prime
-def sum_three(a, b, c):
-    return a + b + c
+def wite_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(word_count):
+            file.write(f'Какое-то слово №{i+1}\n')
+            time.sleep(0.1)
+    print(f'Запись в файл "{file_name}" завершена')
 
 
-result = sum_three(3, 3, 6)
-print(result)
+start = time.time()
+wite_words(10, 'example1.txt')
+wite_words(30, 'example2.txt')
+wite_words(200, 'example3.txt')
+wite_words(100, 'example4.txt')
+end = time.time()
+res_func = (end-start)
+
+
+fi_thr = Thread(target=wite_words, args=(10, 'example5.txt'))
+se_thr = Thread(target=wite_words, args=(30, 'example6.txt'))
+th_thr = Thread(target=wite_words, args=(200, 'example7.txt'))
+fo_thr = Thread(target=wite_words, args=(100, 'example8.txt'))
+
+start = time.time()
+fi_thr.start()
+se_thr.start()
+th_thr.start()
+fo_thr.start()
+
+fi_thr.join()
+se_thr.join()
+th_thr.join()
+fo_thr.join()
+end = time.time()
+res_thr = (end-start)
+
+print(f'Время затраченное одним потоком: {res_func} с\nВремя затраченное мультипотоком: {res_thr} с')
+
