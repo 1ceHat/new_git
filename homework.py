1198ac4b373e24807bea40b1e143dc4de292164a
1ceHat <artemdudorov2015@gmail.com>
2024-07-20 11:44:20 +0500

    module10 5

--- homework.py
+++ homework.py
@@ -1,51 +1,49 @@
-from multiprocessing import Pool
+from multiprocessing import Process, Pool, Manager
 
 
 class WarehouseManager:
 
-    data = {}
+    def __init__(self):
+        self.data = Manager().dict()
 
     def process_request(self, request):
         product, operation, amount = request
         if operation == 'receipt':
-            if product not in WarehouseManager.data:
-                WarehouseManager.data.update({product: amount})
+            if product not in self.data:
+                self.data.update({product: amount})
             else:
-                WarehouseManager.data[product] += amount
+                self.data[product] += amount
             print(f'На склад привезен "{product}" в размере {amount} единиц.')
         elif operation == 'shipment':
-            if product in WarehouseManager.data:
-                if amount > WarehouseManager.data[product]:
+            if product in self.data:
+                if amount > self.data[product]:
                     print('Недостаточно товара на складе!')
                 else:
-                    WarehouseManager.data[product] -= amount
+                    self.data[product] -= amount
                     print(f'Со склада отгружен "{product}" в размере {amount} единиц.')
             else:
                 print('Такого товара на складе нет!')
 
     def run(self, requests):
-        print(requests)
         amount_requests = len(requests)
         processes = []
+        for request in requests:
+            processes.append(Process(target=self.process_request, args=(request,)))
 
-        with Pool(processes=1) as pool:
-            pool.map(self.process_request, requests)
-        # for request in requests:
-        #     self.process_request(request)
-        #     processes.append(Process(target=self.process_request, args=(request,)))
-        #
-        # for process in processes:
-        #     process.start()
-        #     process.join()
+        for process in processes:
+            process.start()
+            process.join()
 
 
 if __name__ == '__main__':
     manager = WarehouseManager()
 
     requests = [
-        ('potato', 'receipt', 10),
-        ('bananas', 'receipt', 10),
-        ('potato', 'shipment', 5)
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
     ]
 
     manager.run(requests)
