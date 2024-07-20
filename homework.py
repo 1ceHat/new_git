b581b9586a6f8fcaba24a1f71ae5b91a7e0c57bf
1ceHat <artemdudorov2015@gmail.com>
2024-07-20 15:07:51 +0500

    module11 2

--- homework.py
+++ homework.py
@@ -1,37 +1,16 @@
-from matplotlib import pyplot as plt
+def introspection_tool(obj):
+    obj_info = {}
+    type_ = type(obj)
+    attributes = dir(obj)
+    methods = []
+    for attr in attributes:
+        type_attr = getattr(obj, attr)
+        if 'method ' in str(type_attr) and '__' not in str(type_attr):
+            methods.append(attr)
+    obj_info.update({'type': type_, 'attributes': attributes, 'methods': methods})
+    return obj_info
 
 
-# N' = -lamda*N
-def f(t, N, lam=0.0866434):  # Функция вычисления значения N' (вероятность радиоактивного распада)
-    return -1 * lam * N
+for key, value in introspection_tool('42').items():
+    print(f"{key}: {value}")
 
-
-def eyler(t0=0, N0=0, t1=16, num=100, lam=1): # Функция эйлера
-    dt = (t1 - t0) / num
-
-    t = [t0]
-    N = [N0]
-
-    for i in range(num):
-        t.append(t[i] + dt)
-        N.append(N[i] + dt * f(t[i], N[i], lam))
-
-    return t, N
-
-
-lam = []
-i = 0.1
-while i < 1:
-    lam.append(i)
-    i += 0.1
-
-t = []
-N = []
-#N0 = int(input('Введите кол-во ядер в момент t=0: '))
-N0 = 10
-for i in lam:
-    t, N = eyler(0, N0, 16, 100, i)
-    plt.plot(t, N) # создаём график со значениями по x и y (t, N)
-    plt.xlabel('Время') # меняем название оси Ох
-    plt.ylabel('Количество ядер') # меняем название оси Оу
-plt.show()
