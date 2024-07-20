e3c8fe14c8d232b44e5e5505e78b98be6d2acd7e
1ceHat <artemdudorov2015@gmail.com>
2024-07-20 16:08:08 +0500

    module12 1

--- homework.py
+++ homework.py
@@ -1,16 +1,35 @@
-def introspection_tool(obj):
-    obj_info = {}
-    type_ = type(obj)
-    attributes = dir(obj)
-    methods = []
-    for attr in attributes:
-        type_attr = getattr(obj, attr)
-        if 'method ' in str(type_attr) and '__' not in str(type_attr):
-            methods.append(attr)
-    obj_info.update({'type': type_, 'attributes': attributes, 'methods': methods})
-    return obj_info
+from runner import Runner
+import unittest
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        test_runner = Runner('F')
+
+        for _ in range(10):
+            test_runner.walk()
+
+        self.assertEqual(test_runner.distance, 50)
+
+    def test_run(self):
+        test_runner = Runner('F')
+
+        for _ in range(10):
+            test_runner.run()
+
+        self.assertEqual(test_runner.distance, 100)
+
+    def test_challenge(self):
+        test_runner1 = Runner('F')
+        test_runner2 = Runner('G')
+
+        for _ in range(10):
+            test_runner1.run()
+            test_runner2.walk()
+
+        self.assertNotEqual(test_runner1.distance, test_runner2)
 
 
-for key, value in introspection_tool('42').items():
-    print(f"{key}: {value}")
+if __name__ == "__main__":
 
+    unittest.main()
