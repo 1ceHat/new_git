1446a262231913b530e85702437a77fc2d1baa5a
1ceHat <artemdudorov2015@gmail.com>
2024-07-20 18:29:04 +0500

    module13 1

--- homework.py
+++ homework.py
@@ -1,51 +1,25 @@
-from runner import Runner
-import unittest
-import logging
-
-class RunnerTest(unittest.TestCase):
-
-    is_frozen = False
-
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
-    def test_walk(self):
-        try:
-            test_runner = Runner('F')
-
-            for _ in range(10):
-                test_runner.walk()
-            logging.info('"test_walk" выполнен успешно!')
-            self.assertEqual(test_runner.distance, 50)
-        except ValueError as e:
-            logging.warning(e)
-
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
-    def test_run(self):
-        try:
-            test_runner = Runner(2)
-
-            for _ in range(10):
-                test_runner.run()
-
-            logging.info('"test_run" выполнен успешно!')
-            self.assertEqual(test_runner.distance, 100)
-        except TypeError as e:
-            logging.warning(e)
-
-    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
-    def test_challenge(self):
-        test_runner1 = Runner('F')
-        test_runner2 = Runner('G')
-
-        for _ in range(10):
-            test_runner1.run()
-            test_runner2.walk()
-
-        self.assertNotEqual(test_runner1.distance, test_runner2)
+from time import sleep, time
+import asyncio
 
 
-logging.basicConfig(level=logging.INFO, filemode="w", filename='runner_test1.log', encoding="utf-8",
-                    format="%(asctime)s || %(levelname)s || %(message)s")
+async def start_programm(name, power):
+    print(f'Силач {name} начал соревнование')
+    for i in range(5):
+        await asyncio.sleep(1/power)
+        print(f'Силач {name} поднял шар номер {i+1}')
 
-if __name__ == "__main__":
-    unittest.main()
+    print(f'Силач {name} закончил соревнования')
+
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_programm('Я', 1))
+    task2 = asyncio.create_task(start_programm('Он', 0.7))
+    task3 = asyncio.create_task(start_programm('Она', 0.1))
+
+    await task1
+    await task2
+    await task3
+
+
+asyncio.run(start_tournament())
 
