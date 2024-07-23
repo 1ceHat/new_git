cdc6f5525504922a04ef2bf8a13c59c1e44c2b00
1ceHat <artemdudorov2015@gmail.com>
2024-07-23 19:10:47 +0500

    module16 5

--- homework.py
+++ homework.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Request, Form, status
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from typing import Annotated
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory='.\\')
 
 users = []
 
@@ -10,16 +13,30 @@ users = []
 class User(BaseModel):
     id: int = None
     username: str
-    age: int
+    age: int = 20
 
 
-@app.get('/users')
-async def get_all_users():
-    return users
+@app.get('/users/{user_id}')
+async def get_user(request: Request, user_id: int) -> HTMLResponse:
+    try:
+        # return users[user_id]
+        for user in users:
+            print(users)
+            if user.id == user_id:
+                return templates.TemplateResponse('users.html', {'request': request, 'user': user})
+        else:
+            raise IndexError
+    except IndexError:
+        return HTTPException(status_code=404, detail='User was not found')
 
 
 @app.post('/users/{username}/{age}')
 async def create_user(user: User):
+    if users:
+        user_id = max(users, key=lambda us: us.id).id+1
+    else:
+        user_id = 1
+    user.id = user_id
     users.append(user)
     return f'User {user.id} is registered.'
 
@@ -49,3 +66,7 @@ async def delete_user(user: User):
             raise IndexError
     except IndexError:
         return HTTPException(status_code=404, detail='User was not found')
+
+@app.get('/')
+async def user_request(request: Request):
+    return templates.TemplateResponse('users.html', {'request': request, "users": users})
