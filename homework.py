9b70ec3ae4e0cc7a76e160962a2369fa3ddd7adb
1ceHat <artemdudorov2015@gmail.com>
2024-07-23 16:14:02 +0500

    module16 3

--- homework.py
+++ homework.py
@@ -3,20 +3,28 @@ from typing import Annotated
 
 app = FastAPI()
 
+users = {'1': 'Имя: Examples, возраст: 18'}
 
-@app.get('/')
-async def main_page():
-    return 'Главная страница'
 
-@app.get('/user/admin')
-async def admin_page():
-    return 'Привет, администратор!'
+@app.get('/users')
+async def get_all_users():
+    return users
 
-@app.get('/user/{user_id}')
-async def user_pages(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='1')):
-    return f'Привет, пользователь номер {user_id}!'
 
-@app.get('/user/{username}/{age}')
-async def user_name_page(username: str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
-                         age: int = Path(ge=18, le=120, description='Enter age', example='25')):
-    return f'Информация о пользователе - Имя: {username},  возраст: {age}'
+@app.post('/users/{username}/{age}')
+async def create_user(username: str, age: int):
+    curr_id = str(int(max(users, key=int))+1)
+    users[curr_id] = f'Имя: {username}, возраст: {age}'
+    return f'User {curr_id} is registered.'
+
+
+@app.put('/users/{user_id}/{username}/{age}')
+async def update_user(user_id: int, username: str, age: int):
+    users[str(user_id)] = f'Имя: {username}, возраст: {age}'
+    return f'The user {user_id} is updated.'
+
+
+@app.delete('/users/{user_id}')
+async def delete_user(user_id: int):
+    users.pop(str(user_id))
+    return f'The user {user_id} is deleted.'
