c141dc1c9a6d40845dafd3f9146b00b49e8ce8db
1ceHat <artemdudorov2015@gmail.com>
2024-07-23 15:39:50 +0500

    module16 2

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -12,9 +13,10 @@ async def admin_page():
     return 'Привет, администратор!'
 
 @app.get('/user/{user_id}')
-async def user_pages(user_id: int):
+async def user_pages(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='1')):
     return f'Привет, пользователь номер {user_id}!'
 
-@app.get('/user')
-async def user_name_page(user_name: str = 'Artem', user_age: int = 20):
-    return f'Информация о пользователе\nИмя: {user_name},  возраст: {user_age}'
+@app.get('/user/{username}/{age}')
+async def user_name_page(username: str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
+                         age: int = Path(ge=18, le=120, description='Enter age', example='25')):
+    return f'Информация о пользователе - Имя: {username},  возраст: {age}'
