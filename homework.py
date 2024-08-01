346fe9239c524a0af82b81d614d11a148ae86e8f
1ceHat <artemdudorov2015@gmail.com>
2024-08-01 13:21:54 +0500

    module16 4 remake

--- homework.py
+++ homework.py
@@ -1,58 +1,72 @@
-class Animal:
-    alive = True
-    fed = False
+from fastapi import FastAPI, Path, HTTPException, Request, Form, status
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Annotated
+from fastapi.templating import Jinja2Templates
 
-    def __init__(self, name):
-        self.name = name
+app = FastAPI()
+templates = Jinja2Templates(directory='.\\')
 
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            print(f"{self.name} съел {food.name}")
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int = 20
+
+
+@app.get('/users/{user_id}')
+async def get_user(request: Request, user_id: int):
+    try:
+        # return users[user_id]
+        for user in users:
+            if user.id == user_id:
+                return templates.TemplateResponse('users.html', {'request': request, 'user': user})
         else:
-            self.alive = False
-            print(f"{self.name} не стал есть {food.name} и умер")
+            raise IndexError
+    except IndexError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
 
 
-class Plant:
-    edible = False
-
-    def __init__(self, name):
-        self.name = name
-
-
-class Mammal(Animal):
-    pass
-
-
-class Predator(Animal):
-    pass
+@app.post('/users/{username}/{age}')
+async def create_user(username: str, age: int):
+    if users:
+        user_id = max(users, key=lambda user: user.id).id+1
+    else:
+        user_id = 1
+    created_user = User(id=user_id, username=username, age=age)
+    users.append(created_user)
+    return f'User {user_id} is registered.'
 
 
-class Flower(Plant):
-    pass
+@app.put('/users/{user_id}/{username}/{age}')
+async def update_user(user: User):
+    try:
+        for exist_user in users:
+            if exist_user.id == user.id:
+                exist_user.username = user.username
+                exist_user.age = user.age
+                return f'The user {user.id} is updated.'
+        else:
+            raise IndexError
+    except IndexError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
 
 
-class Fruit(Plant):
+@app.delete('/users/{user_id}')
+async def delete_user(user_id: int):
+    try:
+        for exist_user in users:
+            if exist_user.id == user_id:
+                index = users.index(exist_user)
+                return f'The user {users.pop(index).id} was deleted.'
+        else:
+            raise IndexError
+    except IndexError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
 
-    def __init__(self, name):
-        self.name = name
-        self.edible = True
 
-
-a1 = Predator('Волк с Уолл-Стрит')
-a2 = Mammal('Хатико')
-p1 = Flower('Цветик семицветик')
-p2 = Fruit('Заводной апельсин')
-
-print(a1.name)
-print(p1.name)
-
-print(a1.alive)
-print(a2.fed)
-a1.eat(p1)
-a2.eat(p2)
-print(a1.alive)
-print(a2.fed)
-
-# Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
+@app.get('/users')
+async def user_request(request: Request):
+    return templates.TemplateResponse('users.html', {'request': request, "users": users})
